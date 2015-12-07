d1f1a8fc61972a060e6ab5fa1edd542f2ebc52cb
Paulo Campos <paulovitorwd@gmail.com>
2015-12-07 18:22:07 -0200

    Finished static UI

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/*===============================
+=            Loaders            =
+===============================*/
+var gulp          = require('gulp');
+var htmlValidator = require('gulp-html-validator');
+var less          = require('gulp-less');
+var jshint        = require('gulp-jshint');
+var jslint        = require('gulp-jslint');
+var browserSync   = require('browser-sync').create();
+/*=====  End of Loaders  ======*/
+
+
+/*==================================
+=            References            =
+==================================*/
+var htmlFiles = './dev/**/*.html';
+var lessFiles = './dev/assets/less/**/*.less';
+var jsFiles   = './dev/assets/js/**/*.js';
+var gulpFile  = './gulpfile.js';
+
+var cssFolder         = './dev/assets/css';
+var validationFolder  = './validation';
+var developmentFolder = './dev';
+/*=====  End of References  ======*/
+
+
+/*======================================
+=            Register tasks            =
+======================================*/
+gulp.task('default', function () {
+
+    gulp.watch(htmlFiles, ['html']);
+    gulp.watch(lessFiles, ['less']);
+    gulp.watch(jsFiles,   ['js']);
+    gulp.watch(gulpFile,  ['gulpfile']);
+});
+
+gulp.task('dev', function () {
+
+    browserSync.init({
+        server: developmentFolder
+    });
+
+    gulp.watch(htmlFiles, ['html']);
+    gulp.watch(lessFiles, ['less']);
+    gulp.watch(jsFiles,   ['js']);
+    gulp.watch(gulpFile,  ['gulpfile']);
+
+    gulp.watch(htmlFiles).on('change', browserSync.reload);
+    gulp.watch(jsFiles).on('change', browserSync.reload);
+});
+/*=====  End of Register tasks  ======*/
+
+
+/*====================================
+=            Config tasks            =
+====================================*/
+gulp.task('html', function () {
+    return gulp.src(htmlFiles)
+        .pipe(htmlValidator({format: 'html'}))
+        .pipe(gulp.dest(validationFolder));
+});
+
+gulp.task('less', function () {
+
+    return gulp.src(lessFiles)
+        .pipe(less())
+        .pipe(gulp.dest(cssFolder))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function () {
+
+    return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jslint({
+            node: true,
+            browser: true
+        }));
+});
+
+gulp.task('gulpfile', function () {
+
+    return gulp.src(gulpFile)
+        .pipe(jshint())
+        .pipe(jslint({
+            node: true,
+            browser: true
+        }));
+});
+/*=====  End of Config tasks  ======*/
