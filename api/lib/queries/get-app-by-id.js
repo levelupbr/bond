31475e2d3c8165dfb1132a30e441787765ae925e
Jeferson Amorim <amorimjj@gmail.com>
2015-12-09 14:06:17 -0200

    Adding memory control

--- api/lib/queries/get-app-by-id.js
+++ api/lib/queries/get-app-by-id.js
@@ -2,14 +2,20 @@
 
 const Q = require('q'),
   App = require('./../models/app');
+  
+let apps = {};  
 
 let getAppById = {
 
       'execute': function (id) {
 
           let defered = Q.defer();
+          
+          if ( apps[id] ) return defered.resolve(apps[id]);
+          
           App.findById(id, function (err, data) {
               if (err||!data) return defered.reject(err);
+              apps[id] = data;
               defered.resolve(data);
           });
 
