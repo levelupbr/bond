8b0a3568179d5eb2c7ff3f73559764c57816f7ce
Jeferson Amorim <amorimjj@gmail.com>
2015-12-09 15:32:05 -0200

    Fixing query

--- api/lib/queries/get-app-by-id.js
+++ api/lib/queries/get-app-by-id.js
@@ -5,20 +5,22 @@ const Q = require('q'),
   
 let apps = {};  
 
+let resolve = function(id, defered) {
+
+    if ( apps[id] ) return defered.resolve(apps[id]);
+
+    App.findById(id, function (err, data) {
+        if (err||!data) return defered.reject(err);
+        apps[id] = data;
+        defered.resolve(data);
+    });
+}
+
 let getAppById = {
 
       'execute': function (id) {
-
           let defered = Q.defer();
-          
-          if ( apps[id] ) return defered.resolve(apps[id]);
-          
-          App.findById(id, function (err, data) {
-              if (err||!data) return defered.reject(err);
-              apps[id] = data;
-              defered.resolve(data);
-          });
-
+          resolve(id, defered);
           return defered.promise;
       }
 };
